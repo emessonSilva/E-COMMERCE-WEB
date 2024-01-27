f40c8917eac77ea79a5b356ccbbd3c93d3a2cdd0
Emesson <emessonbarbosa600@gmail.com>
2024-01-26 23:36:55 -0300

    changes made

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://my-json-server.typicode.com/emessonSilva/E-COMMERCE-WEB",
+  baseURL: "https://my-json-server.typicode.com/augustocesarfmo/rest-crud-web",
   timeout: 1000,
 });
